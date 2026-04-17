69e31f72fba7bc9bc5f63ac59ef4f340d4bd2e17
epdsn <ibestudios@gmail.com>
2026-04-17 10:16:39 -0700

    Initial commit: SVG to icon font build (Fantasticon)

---
+++ .fantasticonrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  inputDir: './svg',
+  outputDir: './dist',
+  name: 'eric-icons',
+  prefix: 'eric',
+  fontTypes: ['woff2', 'woff', 'ttf'],
+  assetTypes: ['css', 'html', 'json'],
+  fontsUrl: '.',
+  fontHeight: 300,
+  normalize: true,
+  descent: 50,
+  tag: 'i',
+};
